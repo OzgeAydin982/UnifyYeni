7a86dee25026e215a4b3471f4c16217bdd7a1e21
Özge Aydın <80550373+OzgeAydin982@users.noreply.github.com>
2021-06-10 22:49:36 +0300

    Update GetRec

--- lib/screens/GetRec.dart
+++ lib/screens/GetRec.dart
@@ -7,11 +7,131 @@ import 'package:unify_app/screens/tabs/recommendation.dart';
 import 'package:unify_app/utils/color.dart';
 
 class GetRecPage extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    // Change Status Bar Color
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: primaryColor),
+    );
+    final pageTitle = Column(
+      //crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Our Recommendation for You!",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+        ),
+        Text(
+          "",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        )
+      ],
+    );
 
+    final emailField = TextFormField(
+      decoration: InputDecoration(
+        labelText: 'User1',
+        labelStyle: TextStyle(color: Colors.white),
+        prefixIcon: Icon(
+          LineIcons.twitter,
+          color: Colors.white,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+    );
 
+    final passwordField = TextFormField(
+      decoration: InputDecoration(
+        labelText: 'User2',
+        labelStyle: TextStyle(color: Colors.white),
+        prefixIcon: Icon(
+          LineIcons.twitter,
+          color: Colors.white,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+      keyboardType: TextInputType.text,
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      obscureText: true,
+    );
+    final userField = TextFormField(
+      decoration: InputDecoration(
+        labelText: 'User2',
+        labelStyle: TextStyle(color: Colors.white),
+        prefixIcon: Icon(
+          LineIcons.twitter,
+          color: Colors.white,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+      keyboardType: TextInputType.text,
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      obscureText: true,
+    );
 
+    final signupForm = Padding(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[emailField, passwordField, userField],
+        ),
+      ),
+    );
+
+    /*final signupBtn = Container(
+      margin: EdgeInsets.only(top: 40.0),
+      height: 60.0,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7.0),
+        border: Border.all(color: Colors.white),
+        color: Colors.white,
+      ),
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => Navigator.pushNamed(context, wordsViewRoute),
+        color: Colors.white,
+        shape: new RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(7.0),
+        ),
+        child: Text(
+          'Sign Up with LinkedIN',
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+    );*/
 
 
     return Scaffold(
@@ -24,9 +144,9 @@ Widget build(BuildContext context) {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("args.userName"),
-              Text("data2"),
-              //getRecomBtn,
+              pageTitle,
+              signupForm,
+              //signupBtn,
             ],
           ),
         ),
